d3add2b9e207a8b3f86ba76481ab77f2431f4fbb
Jose Flavio de Souza Dias Junior <contato@joseflavio.com>
2018-02-28 22:00:35 -0300

    Atualização da URL http://joseflavio.com/copaiba/

--- fonte/com/joseflavio/copaiba/SaidaOutputStream.java
+++ fonte/com/joseflavio/copaiba/SaidaOutputStream.java
@@ -2,7 +2,7 @@
 /*
  *  Copyright (C) 2016-2018 José Flávio de Souza Dias Júnior
  *  
- *  This file is part of Copaíba - <http://www.joseflavio.com/copaiba/>.
+ *  This file is part of Copaíba - <http://joseflavio.com/copaiba/>.
  *  
  *  Copaíba is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -21,7 +21,7 @@
 /*
  *  Direitos Autorais Reservados (C) 2016-2018 José Flávio de Souza Dias Júnior
  * 
- *  Este arquivo é parte de Copaíba - <http://www.joseflavio.com/copaiba/>.
+ *  Este arquivo é parte de Copaíba - <http://joseflavio.com/copaiba/>.
  * 
  *  Copaíba é software livre: você pode redistribuí-lo e/ou modificá-lo
  *  sob os termos da Licença Pública Menos Geral GNU conforme publicada pela
